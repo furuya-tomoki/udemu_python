6d13e8b997f929dc523fdc82c45e7a80dbd3c38d
furuya-tomoki <furuya11221993@gmail.com>
2021-06-30 19:40:36 +0000

    [fix]python.py

--- python1.py
+++ python1.py
@@ -1,40 +1,13 @@
-r = [1, 2, 3, 4, 5, 1, 2, 3]
-# print(r.index(3))
-# 3を検索
-# 出力結果 2 = インデックス番号2に3がある
-print(r.index(3, 3))
- # (3, 3) = インデックス番号3から3を探す
+i = [1, 2, 3, 4, 5]
+j = i
+j[0] = 100
+# インデックス番号0を100に変更
+print("j =", j)
+print("i =", i)
+# 配列内の要素が「i」「j」も変更されてしまう
 
-print(r.count(3))
-# 3の個数確認
-
-if 5 in r:
-    print("exist")
-# 5があったら「exist」と出力
-
-r.sort()
-print(r)
-# r配列を小さい順に並び替え
-
-r.sort()
-r.sort(reverse = True)
-print(r)
-# r配列を大きい順に並び替え
-
-r.reverse()
-print(r)
-# 配列を元に戻す
-
-s = "My name is Mike."
-to_split = s.split(" ")
-print(to_split)
-# 出力結果 = ['My', 'name', 'is', 'Mike.']
-# splitで配列に分ける
-
-x = " ".join(to_split)
-print(x)
-# 出力結果 = My name is Mike.
-# joinメソッド = 文字列の連結
-
-print(help(list))
-# メソッド一覧の確認
+x = [1, 2, 3, 4, 5]
+y = x.copy()
+y[0] = 100
+print("y =",y)
+print("x =",x)
