456ef1f77db43b81c141b67747f1727541f05834
furuya-tomoki <furuya11221993@gmail.com>
2021-07-02 14:33:43 +0000

    [Finish]python2.py

---
+++ python2.py
@@ -0,0 +1,15 @@
+seat = []
+min = 0
+max = 5
+min <= len(seat) < max
+seat.append("p")
+min <= len(seat) < max
+len(seat)
+seat.append("p")
+min <= len(seat) < max
+len(seat)
+seat.append("p")
+seat.append("p")
+min <= len(seat) < max
+seat.append("p")
+
