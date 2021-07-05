ba6855e14af532ae0e1f72ae2f2d7fa2e62c6939
furuya-tomoki <furuya11221993@gmail.com>
2021-07-05 16:23:39 +0000

    [Finish]python9.py

---
+++ python9.py
@@ -0,0 +1,22 @@
+s = "aaaaaaaaaaaaaa"\
+     + "bbbbbbbbbbb"
+     
+print(s)
+
+# 「\」でも「()」でも改行せずに出力可能
+
+d = ("aaaaaaaaaaaaaa"
+     + "bbbbbbbbbbb")
+     
+print(d)
+
+
+x = 1 + 1 + 1 + 1 + 1 + 1\
+      + 1 + 1 + 1 + 1 + 1 + 1
+print(x)
+
+# 「\」でも「()」でも改行せずに出力可能
+
+y = (1 + 1 + 1 + 1 + 1 + 1
+      + 1 + 1 + 1 + 1 + 1 + 1)
+print(y)
