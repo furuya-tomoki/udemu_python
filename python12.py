54a8c7fe3e6563f2eec26df1896a25a5d9c89cc8
furuya-tomoki <furuya11221993@gmail.com>
2021-07-10 11:54:31 +0000

    [Finish]python12.,py

---
+++ python12.py
@@ -0,0 +1,24 @@
+y = [1,2,3]
+x = 1
+
+if x in y:
+    print("in")
+    
+    
+if 100 not in y:
+    print("not in")
+    
+# a = 1
+# b = 2
+
+# if not a == b:
+#     print("Not equal1")
+    
+# if a > b:
+#     print("Not equal2")
+
+is_ok = True
+
+if not is_ok:
+    print("hello")
+    
