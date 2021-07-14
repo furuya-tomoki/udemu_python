2eaae2e190da6bd1ac27375d640f1235781b9a49
furuya-tomoki <furuya11221993@gmail.com>
2021-07-14 14:50:43 +0000

    [Finish]python14.py

---
+++ python13.py
@@ -0,0 +1,14 @@
+is_ok = "sss"
+
+if  is_ok:
+    print("Ok!")
+    # 文字列が入っているか確認
+else:
+    print("No!")
+    
+is_ok = [1,2,3,4]
+
+if  is_ok:
+    print("Ok!")
+else:
+    print("No!")
---
+++ python14.py
@@ -0,0 +1,9 @@
+is_empty = None
+# print(help(is_empty))
+
+if is_empty is not None:
+    print("None!!")
+    
+print(1 == True)
+print(1 is True)
+print(None is True)
