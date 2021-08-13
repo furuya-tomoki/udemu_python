e11c46560b7d7e80ed3ba5c520d4effd9abfb6f2
furuya-tomoki <furuya11221993@gmail.com>
2021-08-13 14:00:24 +0000

    [Finish]paiza10

--- paiza7.py
+++ paiza7.py
@@ -1,192 +1,192 @@
-# 関数を作る
+# # 関数を作る
 
-def say_hello():
-    print("hello world")
+# def say_hello():
+#     print("hello world")
 
-say_hello()
-say_hello()
+# say_hello()
+# say_hello()
 
- 関数の定義方法
-defを使って定義します。
+#  関数の定義方法
+# defを使って定義します。
 
-def hello():
-    print("hello world")
-
-
-定義された関数は、それより下の位置から呼び出すことができます。
-def hello():
-    print("hello world")
-
-hello()
+# def hello():
+#     print("hello world")
 
 
-関数の命名規則
-関数の名前は、次のルールに従って付けます。
-・1文字目：英語または、「_」(アンダーバー)
-・2文字目以降：英語の大文字・小文字・数字「_」(アンダーバー)
+# 定義された関数は、それより下の位置から呼び出すことができます。
+# def hello():
+#     print("hello world")
 
-例：
-・ hello　 1文字目が、英小文字
-・ _attack　 1文字目が、「_」(アンダーバー)
-・ move01 2文字目以降に数字
-・ move_left 2文字目以降「_」(アンダーバー)
-・ moveLeft 2文字目以降に英大文字
-
-慣習として、関数の先頭には大文字を使いません。
-また、動詞を表す単語を使うと、コードがわかりやすくなります。
+# hello()
 
 
-作成したコード
+# 関数の命名規則
+# 関数の名前は、次のルールに従って付けます。
+# ・1文字目：英語または、「_」(アンダーバー)
+# ・2文字目以降：英語の大文字・小文字・数字「_」(アンダーバー)
 
-# 引数と戻り値を追加する
+# 例：
+# ・ hello　 1文字目が、英小文字
+# ・ _attack　 1文字目が、「_」(アンダーバー)
+# ・ move01 2文字目以降に数字
+# ・ move_left 2文字目以降「_」(アンダーバー)
+# ・ moveLeft 2文字目以降に英大文字
 
-def sum(x, y):
-    return x + y
-
-num1 = sum(3, 4)
-print(num1)
-num2 = sum(300, 400)
-print(num2)
-
- 関数定義に引数と戻り値を追加する
-引数と戻り値を追加します。
-
-def sum(x, y):
-    return x + y
-
-num1 = sum(3, 4)
-print(num1)
+# 慣習として、関数の先頭には大文字を使いません。
+# また、動詞を表す単語を使うと、コードがわかりやすくなります。
 
 
-# 九九の表を作成してみよう
+# 作成したコード
 
-def multiply(x, y):
-    return x * y
-for step in range(1, 10):
-  for num in range(1, 10):
-    print(multiply(step, num), end="")
-    if num < 9:
-        print(", ", end="")
-  print("")
+# # 引数と戻り値を追加する
+
+# def sum(x, y):
+#     return x + y
+
+# num1 = sum(3, 4)
+# print(num1)
+# num2 = sum(300, 400)
+# print(num2)
+
+#  関数定義に引数と戻り値を追加する
+# 引数と戻り値を追加します。
+
+# def sum(x, y):
+#     return x + y
+
+# num1 = sum(3, 4)
+# print(num1)
+
+
+# # 九九の表を作成してみよう
+
+# def multiply(x, y):
+#     return x * y
+# for step in range(1, 10):
+#   for num in range(1, 10):
+#     print(multiply(step, num), end="")
+#     if num < 9:
+#         print(", ", end="")
+#   print("")
   
   
   
-  # スコープを理解する
+#   # スコープを理解する
 
-message = "paiza"
-a = 10
-b = 20
+# message = "paiza"
+# a = 10
+# b = 20
 
-def sum(x, y):
-    a = 3
-    global message
-     # グローバル変数を変更した場合のみ記載
-     # global + 変数名
-    message += "paiza"
-    print(message + " " + str(a))
-    return x + y
+# def sum(x, y):
+#     a = 3
+#     global message
+#      # グローバル変数を変更した場合のみ記載
+#      # global + 変数名
+#     message += "paiza"
+#     print(message + " " + str(a))
+#     return x + y
 
-num = sum(a, b)
-print(num)
-print(message + " " + str(a))
-
-
-
-スコープとは、変数の有効範囲が決まっていることです。
-Pythonでは、関数定義の中と外ではローカル変数は分離しています。
-同じ名前の変数があっても、独立しているので、それぞれ違う変数として扱われます。
-
-
-グローバル変数
-スコープがなく、関数定義を超えて利用できます。
-ただし、どこで値が代入されたのか分かりにくくなるので、あまり奨励されません。
+# num = sum(a, b)
+# print(num)
+# print(message + " " + str(a))
 
 
 
-import random
+# スコープとは、変数の有効範囲が決まっていることです。
+# Pythonでは、関数定義の中と外ではローカル変数は分離しています。
+# 同じ名前の変数があっても、独立しているので、それぞれ違う変数として扱われます。
 
-def attack(enemy):
-    print("勇者は、" + enemy + "を攻撃した。")
-    hit = random.randint(1,10)
-    if hit < 6:
-        print(enemy + "に、" + str(hit) + "のダメージを与えた！")
-    else:
-        print("クリティカルヒット！" + enemy + "に、100のダメージを与えた！！")
 
+# グローバル変数
+# スコープがなく、関数定義を超えて利用できます。
+# ただし、どこで値が代入されたのか分かりにくくなるので、あまり奨励されません。
 
 
 
+# import random
 
-enemies = ["スライム", "モンスター", "ドラゴン"]
-for enemy in enemies:
-    # print("勇者は、" + enemy + "を攻撃した。")
-    attack(enemy)
+# def attack(enemy):
+#     print("勇者は、" + enemy + "を攻撃した。")
+#     hit = random.randint(1,10)
+#     if hit < 6:
+#         print(enemy + "に、" + str(hit) + "のダメージを与えた！")
+#     else:
+#         print("クリティカルヒット！" + enemy + "に、100のダメージを与えた！！")
+
+
+
+
+
+# enemies = ["スライム", "モンスター", "ドラゴン"]
+# for enemy in enemies:
+#     # print("勇者は、" + enemy + "を攻撃した。")
+#     attack(enemy)
     
     
-作成したコード１（引数のデフォルト値）
+# 作成したコード１（引数のデフォルト値）
 
-# 引数のデフォルト値
+# # 引数のデフォルト値
 
-def introduce(greeting, name = "村人"):
-    print("私は" + name + "です。" + greeting)
+# def introduce(greeting, name = "村人"):
+#     print("私は" + name + "です。" + greeting)
 
-introduce("こんにちは", "勇者")
-introduce("こんにちは")
-
-
- 作成したコード２（可変長引数）
-
-# 可変長引数
-
-def introduce(greeting, *names):
-    for name in names:
-        print("私は" + name + "です。" + greeting)
-
-introduce("こんにちは", "勇者", "村人", "兵士")
+# introduce("こんにちは", "勇者")
+# introduce("こんにちは")
 
 
-作成したコード３（可変長引数 - 辞書）
+#  作成したコード２（可変長引数）
 
-# 可変長引数 - 辞書
+# # 可変長引数
 
-def introduce(**people):
-    for name, greeting in people.items():
-        print("私は" + name + "です。" + greeting)
+# def introduce(greeting, *names):
+#     for name in names:
+#         print("私は" + name + "です。" + greeting)
 
-introduce(hero = "はじめまして", villager = "こんにちは", soldier = "よろしくお願いします")
-
-
-def battle(*enemies):
-	for enemy in enemies:
-		print("戦士は" + enemy + "と戦った")
-
-battle("スライム", "モンスター", "ドラゴン")
+# introduce("こんにちは", "勇者", "村人", "兵士")
 
 
+# 作成したコード３（可変長引数 - 辞書）
 
-引数のデフォルト値とは
-関数定義のところで、引数にデフォルト値を指定できます。
-引数呼び出しで、引数を省略した場合、このデフォルト値が適用されます。
+# # 可変長引数 - 辞書
 
-# キーワード引数
+# def introduce(**people):
+#     for name, greeting in people.items():
+#         print("私は" + name + "です。" + greeting)
 
-def say_hello(greeting = "hello", target = "world"):
-    print(greeting + " " + target)
+# introduce(hero = "はじめまして", villager = "こんにちは", soldier = "よろしくお願いします")
 
-say_hello()                      # hello world
-say_hello("こんにちは", "皆さん")   # こんにちは 皆さん
+
+# def battle(*enemies):
+# 	for enemy in enemies:
+# 		print("戦士は" + enemy + "と戦った")
+
+# battle("スライム", "モンスター", "ドラゴン")
 
 
 
-# キーワード引数
+# 引数のデフォルト値とは
+# 関数定義のところで、引数にデフォルト値を指定できます。
+# 引数呼び出しで、引数を省略した場合、このデフォルト値が適用されます。
 
-def say_hello(greeting = "hello", target = "world"):
-    print(greeting + " " + target)
+# # キーワード引数
 
-say_hello()
-say_hello("こんにちは", "皆さん")
-say_hello("good morning!")
-say_hello(greeting = "ネコ先生", target = "皆さん")
-say_hello(target = "ネコ先生", greeting = "おはようございます")
-say_hello(target = "ネコ先生")
-say_hello(greeting = "おはようございます")
+# def say_hello(greeting = "hello", target = "world"):
+#     print(greeting + " " + target)
+
+# say_hello()                      # hello world
+# say_hello("こんにちは", "皆さん")   # こんにちは 皆さん
+
+
+
+# # キーワード引数
+
+# def say_hello(greeting = "hello", target = "world"):
+#     print(greeting + " " + target)
+
+# say_hello()
+# say_hello("こんにちは", "皆さん")
+# say_hello("good morning!")
+# say_hello(greeting = "ネコ先生", target = "皆さん")
+# say_hello(target = "ネコ先生", greeting = "おはようございます")
+# say_hello(target = "ネコ先生")
+# say_hello(greeting = "おはようございます")
--- paiza9.py
+++ paiza9.py
@@ -1,249 +1,249 @@
- ＃02:クラスを継承する 
-ここでは、クラスの継承ついて学習します。例として、RPGで使うアイテムが入る宝箱クラスを作り、そこから宝石箱クラスを継承で作ってみましょう
+#  ＃02:クラスを継承する 
+# ここでは、クラスの継承ついて学習します。例として、RPGで使うアイテムが入る宝箱クラスを作り、そこから宝石箱クラスを継承で作ってみましょう
 
-class Box:
-    def __init__(self, item):
-        self.item = item
+# class Box:
+#     def __init__(self, item):
+#         self.item = item
 
-    def open(self):
-        print("宝箱を開いた。" + self.item + "を手に入れた。")
+#     def open(self):
+#         print("宝箱を開いた。" + self.item + "を手に入れた。")
 
-class JewelryBox(Box):
-    def look(self):
-        print("宝箱はキラキラと輝いている。")
+# class JewelryBox(Box):
+#     def look(self):
+#         print("宝箱はキラキラと輝いている。")
 
-box = Box("鋼鉄の剣")
-box.open()
+# box = Box("鋼鉄の剣")
+# box.open()
 
-jewelrybox = JewelryBox("魔法の指輪")
-jewelrybox.look()
-jewelrybox.open()
+# jewelrybox = JewelryBox("魔法の指輪")
+# jewelrybox.look()
+# jewelrybox.open()
 
 
-class Greeting:
-    def __init__(self):
-        self.msg = "hello"
-        self.target = "paiza"
+# class Greeting:
+#     def __init__(self):
+#         self.msg = "hello"
+#         self.target = "paiza"
 
-class Hello(Greeting):
-    # この下に、say_helloメソッドを記述する
-    def say_hello(self):
-        print(self.msg + " " + self.target)
+# class Hello(Greeting):
+#     # この下に、say_helloメソッドを記述する
+#     def say_hello(self):
+#         print(self.msg + " " + self.target)
     
-player = Hello()
-player.say_hello()
+# player = Hello()
+# player.say_hello()
 
 
 
-メソッドのオーバーライドとは
-オーバーライドを利用すると、スーパークラスが持つメソッドを、サブクラスで書き換えて再定義することができます。
+# メソッドのオーバーライドとは
+# オーバーライドを利用すると、スーパークラスが持つメソッドを、サブクラスで書き換えて再定義することができます。
 
 
-class Box:
-    def __init__(self, item):
-        self.item = item
+# class Box:
+#     def __init__(self, item):
+#         self.item = item
 
-    def open(self):
-        print("宝箱を開いた。" + self.item + "を手に入れた。")
+#     def open(self):
+#         print("宝箱を開いた。" + self.item + "を手に入れた。")
 
-class MagicBox(Box):
-    def look(self):
-        print("宝箱は、妖しく輝いている。")
+# class MagicBox(Box):
+#     def look(self):
+#         print("宝箱は、妖しく輝いている。")
 
-    def open(self):
-        print("宝箱を開いた。" + self.item + "が襲ってきた！")
+#     def open(self):
+#         print("宝箱を開いた。" + self.item + "が襲ってきた！")
 
-box = Box("鋼鉄の剣")
-box.open()
+# box = Box("鋼鉄の剣")
+# box.open()
 
-magicbox = MagicBox("モノマネモンスター")
-magicbox.look()
-magicbox.open()
-
-
-class Greeting:
-    def __init__(self):
-        self.msg = "hello"
-        self.target = "paiza"
-
-    def say_hello(self):
-        print(self.msg + " " + self.target)
-
-class Hello(Greeting):
-    # ここにオーバライドするメソッドを記述する
-    def say_hello(self, target):
-        print(self.msg + " " + target)
-         # 「self.target」だと「python」が入る場所がないので「target」で定義する
+# magicbox = MagicBox("モノマネモンスター")
+# magicbox.look()
+# magicbox.open()
 
 
-player = Hello()
-player.say_hello("python")
+# class Greeting:
+#     def __init__(self):
+#         self.msg = "hello"
+#         self.target = "paiza"
+
+#     def say_hello(self):
+#         print(self.msg + " " + self.target)
+
+# class Hello(Greeting):
+#     # ここにオーバライドするメソッドを記述する
+#     def say_hello(self, target):
+#         print(self.msg + " " + target)
+#          # 「self.target」だと「python」が入る場所がないので「target」で定義する
 
 
-# メソッドをオーバーライドしよう2
+# player = Hello()
+# player.say_hello("python")
 
-class Greeting:
-    def __init__(self):
-        self.msg = "hello"
-        self.target = "paiza"
+
+# # メソッドをオーバーライドしよう2
+
+# class Greeting:
+#     def __init__(self):
+#         self.msg = "hello"
+#         self.target = "paiza"
     
-    def say_hello(self):
-        print(self.msg + " " + self.target)
+#     def say_hello(self):
+#         print(self.msg + " " + self.target)
 
-class Hello(Greeting):
-    def say_hello(self):
-        print(self.msg + " " + self.target)
-        print("YEAH YEAH YEAH!")
-
-
-player = Hello()
- # 「Greeting」から「Hello」に変更するだけで、出力結果も変わる
-player.say_hello()
+# class Hello(Greeting):
+#     def say_hello(self):
+#         print(self.msg + " " + self.target)
+#         print("YEAH YEAH YEAH!")
 
 
+# player = Hello()
+#  # 「Greeting」から「Hello」に変更するだけで、出力結果も変わる
+# player.say_hello()
 
-class Player:
-    def __init__(self, name):
-        self.name = name
 
-    def attack(self, enemy):
-        print(self.name + "は、" + enemy + "を攻撃した！")
 
-print("=== パーティーでスライムと戦う ===")
-hero = Player("勇者")
-# hero.attack("スライム")
-warrior = Player("戦士")
+# class Player:
+#     def __init__(self, name):
+#         self.name = name
 
-party = [hero, warrior]
-for member in party:
-    member.attack("スライム")
-    
-    
-    
-class Player:
-    def __init__(self, name):
-        self.name = name
+#     def attack(self, enemy):
+#         print(self.name + "は、" + enemy + "を攻撃した！")
 
-    def attack(self, enemy):
-        print(self.name + "は、" + enemy + "を攻撃した！")
+# print("=== パーティーでスライムと戦う ===")
+# hero = Player("勇者")
+# # hero.attack("スライム")
+# warrior = Player("戦士")
 
-class Wizard(Player):
-    def attack(self, enemy):
-        print("ズバーン！")
-        print(self.name + "は、" + enemy + "に炎を放った！")
-
-print("=== パーティーでスライムと戦う ===")
-hero = Player("勇者")
-# hero.attack("スライム")
-warrior = Player("戦士")
-wizard = Wizard("魔法使い")
-
-party = [hero, warrior, wizard]
-for member in party:
-    member.attack("スライム")
+# party = [hero, warrior]
+# for member in party:
+#     member.attack("スライム")
     
     
-# coding: utf-8
-# RPGの攻撃シーン
+    
+# class Player:
+#     def __init__(self, name):
+#         self.name = name
 
-class Player:
-    def __init__(self, name):
-        self.name = name
+#     def attack(self, enemy):
+#         print(self.name + "は、" + enemy + "を攻撃した！")
 
-    def attack(self, enemy):
-        print(self.name + "は" + enemy + "を攻撃した")
+# class Wizard(Player):
+#     def attack(self, enemy):
+#         print("ズバーン！")
+#         print(self.name + "は、" + enemy + "に炎を放った！")
 
-class Warrior(Player):
-    def attack(self, enemy):
-        print(self.name + "は" + enemy + "を猛攻撃した")
+# print("=== パーティーでスライムと戦う ===")
+# hero = Player("勇者")
+# # hero.attack("スライム")
+# warrior = Player("戦士")
+# wizard = Wizard("魔法使い")
 
-
-team = []
-team.append(Player("勇者"))
-team.append(Player("魔法使い"))
-team.append(Warrior("戦士"))
-
-for person in team:
-    person.attack("スライム")
-
-
-
-class Player:
-    def __init__(self, name):
-        self.name = name
-
-    def attack(self, enemy):
-        print(self.name + "は、" + enemy + "を攻撃した！")
-
-class Wizard(Player):
-    def __init__(self):
-        super().__init__("魔法使い")
-
-    def attack(self, enemy):
-        self.__spell()
-        print(self.name + "は、" + enemy + "に炎を放った！")
-
-    def __spell(self):
-        print("ズバーン！")
-
-print("=== パーティーでスライムと戦う ===")
-hero = Player("勇者")
-# hero.attack("スライム")
-warrior = Player("戦士")
-wizard = Wizard()
-
-party = [hero, warrior, wizard]
-for member in party:
-    member.attack("スライム")
-
-# wizard.__spell()
-
-
-
- Player.charactor_count += 1
-
-Player.charactor_count = Player.charactor_count + 1
-
-を、省略する記法です。「-=」「/=」なども使用できます。
-
-
-クラス変数
-オブジェクト間で共通して利用できる変数のこと。
-
-
-クラスメソッド
-オブジェクト間で共通して利用できるメソッドのこと。
-動画では、2つの方法でクラスメソッドを作成しましたが、デコレータを使用する方法が推奨されています。
-
-
-
- デコレータ
-すでに定義されている関数に、新たに機能を追加できる仕組みのこと。
-デコレータは、すでに用意されていたり、自分で作成したりすることができます。
-
-動画では、@classmethodを使用しました。
-
-
-@classmethod
-def summary(cls):
-    print(str(Player.__charactor_count) + "人で、スライムを攻撃した。")
+# party = [hero, warrior, wizard]
+# for member in party:
+#     member.attack("スライム")
     
     
-    def summary(cls):
-    print(str(Player.__charactor_count) + "人で、スライムを攻撃した。")
+# # coding: utf-8
+# # RPGの攻撃シーン
 
-summary = classmethod(summary)
+# class Player:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def attack(self, enemy):
+#         print(self.name + "は" + enemy + "を攻撃した")
+
+# class Warrior(Player):
+#     def attack(self, enemy):
+#         print(self.name + "は" + enemy + "を猛攻撃した")
+
+
+# team = []
+# team.append(Player("勇者"))
+# team.append(Player("魔法使い"))
+# team.append(Warrior("戦士"))
+
+# for person in team:
+#     person.attack("スライム")
 
 
 
-from datetime import datetime, timedelta, timezone
+# class Player:
+#     def __init__(self, name):
+#         self.name = name
 
-jst   = timezone(timedelta(hours=9))
-today = datetime.now(jst)
-print(today)
-print(today.year)
-print(today.month)
-print(today.day)
+#     def attack(self, enemy):
+#         print(self.name + "は、" + enemy + "を攻撃した！")
 
-day = datetime.strptime("2030/01/10 06:02:19", "%Y/%m/%d %H:%M:%S")
-print(day)
+# class Wizard(Player):
+#     def __init__(self):
+#         super().__init__("魔法使い")
+
+#     def attack(self, enemy):
+#         self.__spell()
+#         print(self.name + "は、" + enemy + "に炎を放った！")
+
+#     def __spell(self):
+#         print("ズバーン！")
+
+# print("=== パーティーでスライムと戦う ===")
+# hero = Player("勇者")
+# # hero.attack("スライム")
+# warrior = Player("戦士")
+# wizard = Wizard()
+
+# party = [hero, warrior, wizard]
+# for member in party:
+#     member.attack("スライム")
+
+# # wizard.__spell()
+
+
+
+#  Player.charactor_count += 1
+
+# Player.charactor_count = Player.charactor_count + 1
+
+# を、省略する記法です。「-=」「/=」なども使用できます。
+
+
+# クラス変数
+# オブジェクト間で共通して利用できる変数のこと。
+
+
+# クラスメソッド
+# オブジェクト間で共通して利用できるメソッドのこと。
+# 動画では、2つの方法でクラスメソッドを作成しましたが、デコレータを使用する方法が推奨されています。
+
+
+
+#  デコレータ
+# すでに定義されている関数に、新たに機能を追加できる仕組みのこと。
+# デコレータは、すでに用意されていたり、自分で作成したりすることができます。
+
+# 動画では、@classmethodを使用しました。
+
+
+# @classmethod
+# def summary(cls):
+#     print(str(Player.__charactor_count) + "人で、スライムを攻撃した。")
+    
+    
+#     def summary(cls):
+#     print(str(Player.__charactor_count) + "人で、スライムを攻撃した。")
+
+# summary = classmethod(summary)
+
+
+
+# from datetime import datetime, timedelta, timezone
+
+# jst   = timezone(timedelta(hours=9))
+# today = datetime.now(jst)
+# print(today)
+# print(today.year)
+# print(today.month)
+# print(today.day)
+
+# day = datetime.strptime("2030/01/10 06:02:19", "%Y/%m/%d %H:%M:%S")
+# print(day)
