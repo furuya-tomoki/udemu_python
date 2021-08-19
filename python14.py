aaccc33ca8f46984df06b131f9c15246540de016
furuya-tomoki <furuya11221993@gmail.com>
2021-08-19 14:52:56 +0000

    [Add]test

--- python14.py
+++ python14.py
@@ -1,9 +1,36 @@
-is_empty = None
-# print(help(is_empty))
+num = [[[0 for i in range(3)] for j in range(5)] for k in range(3)]
 
-if is_empty is not None:
-    print("None!!")
+for k in range(3):
+    num[0][0][k] = 1
     
-print(1 == True)
-print(1 is True)
-print(None is True)
+for j in range(5):
+    num[0][j][1] = 1
+    
+for j in range(3):
+    num[1][0][k] = 1
+    num[1][2][k] = 1
+    
+for j in range(5):
+    num[1][j][0] = 1
+    if j < 3:
+        num[1][j][2] = 1
+        
+num[1][0][1] = 1
+num[1][2][1] - 1
+
+for i in range(5):
+    num[2][j][0] = 1
+    if(j == 0 or j == 4):
+        num[2][j][1] = 1
+    else:
+        num[2][j][2] = 1
+        
+for i in range(3):
+    for j in range(5):
+        for k in range(3):
+            if(num[i][j][k] == 1):
+                print("■", end =" ")
+            else:
+                print("□", end =" ")
+        print("")
+    print("")
--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-# 1から50までの和を計算して表示
-num = 0
-for i in range(1, 51):
-    num += i
-print(num)
-
-
-
+i = 0
+while i < 10:
+    print(i)
+    i += 1
