37659b92dcd5aa4bc0b2e29fd6f8647c8ba89a59
furuya-tomoki <furuya11221993@gmail.com>
2021-08-27 08:26:49 +0000

    [Add]test4.py

--- test3.py
+++ test3.py
@@ -1,4 +1,8 @@
 input_lines = input()
 new = input_lines.translate(str.maketrans("54321", "ABCDE"))
+ # 複数の文字（長さ1の文字列）を指定して置換する場合は文字列（str型）のtranslate()メソッドを使う。
+ # translate()に指定する変換テーブルはstr.maketrans()関数で作成する。
+ # 複数の文字（長さ1の文字列）を置換する場合はtranslate()
+ # str.maketrans()関数には置換元文字をキー、置換先文字列を値とする辞書を指定する。
 
 print(new)
---
+++ test4.py
@@ -0,0 +1,8 @@
+# 文字を縦から横にする
+s = ""
+for i in range(int(input())):
+    # rangeで数字を代入
+    s += input()
+    # 変数sに回数分の文字を＋
+
+print(s)
